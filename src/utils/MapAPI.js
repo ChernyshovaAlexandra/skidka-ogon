c464c26046c1e8cb4c59cf1d86791dcb38762206
Alexandra <alexandrachrystal@gmail.com>
2023-06-20 17:23:26 +0400

    Updates

--- src/utils/MapAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://dixy.ru/api/v1",  ///STORES?region_id=?region_id=
-    responseType: "json",
-    headers: { 'Content-Type': 'application/json'}
-});
